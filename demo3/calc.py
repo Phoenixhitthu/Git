8522a9059c9266d96670ea060abba276ddbae662
Hithendra <87265007+Phoenixhitthu@users.noreply.github.com>
2022-10-20 10:43:19 +0530

    update

--- demo3/calc.py
+++ demo3/calc.py
@@ -4,8 +4,8 @@ c=10
 sum=a+b+c
 product=a*b*c
 diff=b-a
-maxi=max(a,b,c)
-print("MAX:",maxi)
+div=b/a
+print("division:",div)
 print("SUm is:",sum)
 print("difference of a and b is:",diff)
 print("Product is:",product)
