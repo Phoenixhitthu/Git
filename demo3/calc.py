6b3e856a47e2332a9c9d57c7ba73627df78e6382
Hithendra <87265007+Phoenixhitthu@users.noreply.github.com>
2022-10-20 10:33:28 +0530

    Initial commit

---
+++ demo3/calc.py
@@ -0,0 +1,14 @@
+a=10
+b=20
+c=10
+sum=a+b+c
+product=a*b*c
+diff=b-a
+maxi=max(a,b,c)
+mini=min(a,b,c)
+logi=
+print("MIN:",mini)
+print("MAX:",maxi)
+print("SUm is:",sum)
+print("difference of a and b is:",diff)
+print("Product is:",product)
